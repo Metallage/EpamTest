9ff21c88d403570723b0499edead12d4c86965bc
Egor Timashev <metallagetemp@gmail.com>
2019-10-28 12:45:01 +0300

    FXD comments

--- powershell/bootstrap_cake.ps1
+++ powershell/bootstrap_cake.ps1
@@ -1,12 +1,16 @@
+#Список файлов необходимых ддя работы cake-скрипта
 $CakeFiles = @("build.cake", "Settings.json", "CakeSettingsHelper.dll")
+#дирректория с cake-скриптом
 $CakeDir = "cake"
 
+#Проверка на существование дирректории с cake
 $DirExists = Test-Path -Path $CakeDir
 
 if($DirExists -eq "True")
 {
     Set-Location $CakeDir
 
+    #проверка на наличие необходимых файлов
     foreach ($Cakefile in $CakeFiles) 
     {
         $FileExists = Test-Path -Path $CakeFile
@@ -17,10 +21,11 @@ if($DirExists -eq "True")
         }
     }
 
+    #Скачиваем build.ps1 для cake-скрипта
     Invoke-WebRequest https://cakebuild.net/download/bootstrapper/windows -OutFile build.ps1    
 }
 else 
 {
-    throw $CakeDir+ " не обнаружена!!!" 
+    throw $CakeDir + " не обнаружена!!!" 
     exit 1
 }
